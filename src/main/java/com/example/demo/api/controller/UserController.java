ee2c005db3d05bf27235b16a7e0188acadc046a2
Adrian <Adrian@DESKTOP-GLB89L8.home>
2019-10-17 21:41:47 +0200

    Add new method

--- src/main/java/com/example/demo/api/controller/UserController.java
+++ src/main/java/com/example/demo/api/controller/UserController.java
@@ -1,19 +1,25 @@
 package com.example.demo.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.websocket.server.PathParam;
+
+import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.api.model.User;
 import com.example.demo.api.repository.UserRepository;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8000")
 public class UserController {
  
     // standard constructors
@@ -39,5 +45,16 @@ public class UserController {
     @PostMapping("/users")
     void addUser(@RequestBody User user) {
         userRepository.save(user);
+    }
+    
+    @GetMapping("/user/{id}")
+    public Optional<User> getUser(@PathVariable String id) {
+    	try {
+    		System.out.println("ID: " + id);
+        	return userRepository.findById(Long.parseLong(id));
+    	} catch (Exception e) {
+			return null;
+		}
+
     }
 }
